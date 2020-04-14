0e1e9b16090ede902e6669c680cae77cd4417925
MARYRIVERO <mariyndlvalle892286@gmail.com>
2020-04-14 09:04:46 -0500

    Agrega configuración de Firebase

---
+++ beauty-corp/src/firebase/configuration.js
@@ -0,0 +1,10 @@
+
+export default {
+    apiKey: "AIzaSyBMy1K2LDXkjWoX4uK3a7cYwergYCq5GYg",
+    authDomain: "beauty-corp-c6075.firebaseapp.com",
+    databaseURL: "https://beauty-corp-c6075.firebaseio.com",
+    projectId: "beauty-corp-c6075",
+    storageBucket: "beauty-corp-c6075.appspot.com",
+    messagingSenderId: "1013710131201",
+    appId: "1:1013710131201:web:644f8fb784ccd7fb68b000"
+  };
--- beauty-corp/src/index.js
+++ beauty-corp/src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import FirebaseConfig from './firebase/configuration';
+import { FirebaseAppProvider } from 'reactfire';
 
-ReactDOM.render(
+
+ReactDOM.render((
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <FirebaseAppProvider FirebaseConfig={FirebaseConfig}>
+      <Suspense fallback={'Conectando la app...'}>
+        <App />
+       </Suspense>
+    </FirebaseAppProvider>
+  </React.StrictMode>
+),document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
