514ca9ba08ade4efac25e852948c2259c31b7095
MARYRIVERO <mariyndlvalle892286@gmail.com>
2020-04-14 09:55:03 -0500

    Agrega login y actualiza repo

--- beauty-corp/src/index.js
+++ beauty-corp/src/index.js
@@ -1,21 +1,15 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import FirebaseConfig from './firebase/configuration';
-import { FirebaseAppProvider } from 'reactfire';
 
-
-ReactDOM.render((
+ReactDOM.render(
   <React.StrictMode>
-    <FirebaseAppProvider FirebaseConfig={FirebaseConfig}>
-      <Suspense fallback={'Conectando la app...'}>
-        <App />
-       </Suspense>
-    </FirebaseAppProvider>
-  </React.StrictMode>
-),document.getElementById('root'));
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
