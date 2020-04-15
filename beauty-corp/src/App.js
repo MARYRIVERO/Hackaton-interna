4e284d6c61c6f0309e86cb0a1d17eef33ca8295d
MARYRIVERO <mariyndlvalle892286@gmail.com>
2020-04-14 23:25:24 -0500

    Actualiza repo

--- beauty-corp/src/App.js
+++ beauty-corp/src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import Routes from './Routes';
-<<<<<<< HEAD
-=======
 import Header from './components/Header';
->>>>>>> eb92ce0fd0e2281e09795654f26c3159d46f3693
 import './App.css';
 
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
-=======
       <Header />
->>>>>>> eb92ce0fd0e2281e09795654f26c3159d46f3693
       <Routes />
     </div>
   );
--- beauty-corp/src/controller/firebase-login.js
+++ beauty-corp/src/controller/firebase-login.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> eb92ce0fd0e2281e09795654f26c3159d46f3693
 import firebase from '../firebase/firebase';
 
 export const signIn = (email, password) => firebase.auth()
