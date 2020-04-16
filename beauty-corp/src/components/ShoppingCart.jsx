226650e440563276d56256c789a9e879ffaea2c2
nesnip <hpmarines12@gmail.com>
2020-04-16 15:03:39 -0500

    add ShoppingCart view styles + sites data

--- beauty-corp/src/components/ShoppingCart.jsx
+++ beauty-corp/src/components/ShoppingCart.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Orders from './Orders';
+import arrow from '../img/arrow-left.svg';
 
 const ShoppingCart = () => {
   const dataOrders = JSON.parse(localStorage.getItem('arrayProducts'));
   console.log(JSON.parse(localStorage.getItem('arrayProducts')));
   
   return (
-    <main>
+    <main className="shop-cart-container">
+      <section className="title-container">
+        <img src={arrow} alt="regresar"/>
+        <h2 className="title">BOLSA DE COMPRAS</h2>
+      </section>
       <Orders dataOrders={dataOrders}/>
     </main>
   );
