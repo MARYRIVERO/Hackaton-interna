5e90de4305336796d271cd2b8898ce134e58fc13
yamiquispe <yamipqm@gmail.com>
2020-04-16 00:23:12 -0500

    Header responsivo en todas las vistas

--- beauty-corp/src/components/MenuNav.jsx
+++ beauty-corp/src/components/MenuNav.jsx
@@ -12,38 +12,40 @@ const MenuNav = () => {
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <a className="navbar-brand" href="./"><img src={imgLogo} className="imgLogo"/></a>
-            <a className="navbar-brand" href="./"><img src={imgSearch}/></a>
-            <a className="navbar-brand" href="./"><img src={imgUser}/></a>
-            <a className="navbar-brand" href="./"><img src={imgPurse}/></a>
+            <a className="navbar-brand" href="./"><img src={imgLogo} className="imgLogo" /></a>
+            <a className="navbar-brand" href="./"><img src={imgSearch} /></a>
+            <a className="navbar-brand" href="./"><img src={imgUser} /></a>
+            <a className="navbar-brand" href="./"><img src={imgPurse} /></a>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a className="navbar-brand" href="./">Esika</a>
                 <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li className="nav-item active">
+                    <li className="nav-item active text-center">
+                        <a className="navbar-brand text-center" href="./">Esika</a>
+                    </li>
+                    <li className="nav-item active text-center">
                         <a className="nav-link" href="./">Ofertas imperdibles</a>
                     </li>
-                    <li className="nav-item active">
+                    <li className="nav-item active text-center">
                         <a className="nav-link" href="./">Maquillaje</a>
                     </li>
-                    <li className="nav-item active">
+                    <li className="nav-item active text-center">
                         <a className="nav-link" href="./">Cuidado personal</a>
                     </li>
-                    <li className="nav-item active">
+                    <li className="nav-item active text-center">
                         <a className="nav-link" href="./">Tratamiento corporal</a>
                     </li>
-                    <li className="nav-item active">
+                    <li className="nav-item active text-center">
                         <a className="nav-link" href="./">Tratamiento facial</a>
                     </li>
-                    <li className="nav-item active">
+                    <li className="nav-item active text-center">
                         <a className="nav-link" href="./">Fragancias</a>
                     </li>
-                    <li className="nav-item active">
+                    <li className="nav-item active text-center">
                         <a className="nav-link" href="./">Accesorios cosméticos</a>
                     </li>
-                    <li className="nav-item active">
+                    <li className="nav-item active text-center">
                         <a className="nav-link" href="./">Tiendas retail</a>
                     </li>
-                    <li className="nav-item active">
+                    <li className="nav-item active text-center">
                         <a className="nav-link" href="./">Catálogo virtual</a>
                     </li>
                 </ul>
