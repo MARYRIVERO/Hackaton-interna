2abbd5e679943a285a7a0c2f055775d3e3166ce4
nesnip <hpmarines12@gmail.com>
2020-04-17 03:21:26 -0500

    add Login + Register components

--- beauty-corp/src/Routes.jsx
+++ beauty-corp/src/Routes.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Register from './components/Register';
 import Home from './components/Home';
 import ShoppingCart from './components/ShoppingCart';
 import Header from './components/Header';
@@ -15,6 +16,7 @@ const Routes = () => {
       <Switch>
         <Route exact path='/'><Home type={type} category={category} /></Route>
         <Route path='/Login' component={Login} />
+        <Route path='/Register' component={Register} />
         <Route path='/ShoppingCart' component={ShoppingCart} />
       </Switch>
     </BrowserRouter>
